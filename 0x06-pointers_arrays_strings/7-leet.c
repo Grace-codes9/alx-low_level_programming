d68f3e421f792b4914912d35d6722fa89ea5b78e
Grace-codes9 <126185416+Grace-codes9@users.noreply.github.com>
2023-03-31 01:44:22 +0100

    added

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,13 +11,13 @@ char *leet(char *blessing)
 	int a = 0;
 	int b;
 	char leo[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	char ola[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char grace[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
 	while (blessing[a])
 	{
 		for (b = 0; b < 10; b++)
 		{
-			if (blessing[a] == ola[b])
+			if (blessing[a] == grace[b])
 			{
 				blessing[a] = leo[b];
 			}
